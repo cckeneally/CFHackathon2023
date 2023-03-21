89c352ed1a62aa2e13fe15169818c6e5ff49b09e
Rob Edwards <raedwards@gmail.com>
2023-03-21 15:04:06 +1030

    Adding E value checks

--- parse_m8/non_overlapping_mmseqs_hits.py
+++ parse_m8/non_overlapping_mmseqs_hits.py
@@ -10,9 +10,13 @@ from hackathon_lib import stream_blast_results
 __author__ = 'Rob Edwards'
 
 
-def non_overlapping_mmseqs(m8file, mineval, verbose=False):
+def non_overlapping_mmseqs(m8file: str, mineval: float, evalue_check: bool = False, verbose: bool = False):
     """
     Find non-overlapping hits
+    :param m8file: the mmseqs2 m8-format output file
+    :param mineval: the minimum evalue
+    :param evalue_check: check whether discarded hits have a better e value than stored hits
+    :param verbose: more output
     """
 
     hits = []
@@ -38,9 +42,10 @@ def non_overlapping_mmseqs(m8file, mineval, verbose=False):
         for i in range(qsta, qend + 1):
             if i in positions:
                 keep = False
-                if r.evalue < positions[i].evalue:
-                    print(f"We had a hit for {positions[i].query} -> {positions[i].db} : {positions[i].evalue} and now",
-                          f" {r.query} -> {r.db} : {r.evalue}", file=sys.stderr)
+                if evalue_check:
+                    if r.evalue < positions[i].evalue:
+                        print(f"We had a hit for {positions[i].query} -> {positions[i].db} : {positions[i].evalue} and now",
+                              f" {r.query} -> {r.db} : {r.evalue}", file=sys.stderr)
         if keep:
             for i in range(qsta, qend + 1):
                 positions[i] = r
@@ -57,7 +62,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse m8 format mmseqs results')
     parser.add_argument('-f', help='mmseqs m8 format output file', required=True)
     parser.add_argument('-e', help=f'minimum e value [Default: %(default)f]', default=1e-5, type=float)
+    parser.add_argument('-c', help='Check whether better e values are discarded', action='store_true')
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
-    non_overlapping_mmseqs(args.f, args.e, args.v)
+    non_overlapping_mmseqs(args.f, args.e, args.c, args.v)
